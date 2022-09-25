69ddef56f3322543ea24d75ff935e444ecb9dea4
JordanR <jar5927@rit.edu>
2022-09-25 12:08:57 -0400

    Fixing CircleCI error

--- src/server.js
+++ src/server.js
@@ -6,19 +6,13 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const handlePost = (request, response, parsedUrl) => {
-  if (parsedUrl.pathname === '/addUser') {
-    return parseBody(request, response, jsonHandler.addUser);
-  }
-};
-
 // requestHandler = function to call once body is built
 const parseBody = (request, response, requestHandler) => {
   const body = [];
 
   // error recieved
   request.on('error', () => {
-    //console.dir(err);
+    // console.dir(err);
     response.statusCode = 400;
     response.end();
   });
@@ -38,12 +32,18 @@ const parseBody = (request, response, requestHandler) => {
   });
 };
 
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, jsonHandler.addUser);
+  }
+};
+
 const handleGet = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/style.css') {
     htmlHandler.getCSS(request, response);
-  } else if (parsedUrl.pathname === '/'){
+  } else if (parsedUrl.pathname === '/') {
     htmlHandler.getIndex(request, response);
-  }  else if (parsedUrl.pathname === '/client.html'){
+  } else if (parsedUrl.pathname === '/client.html') {
     htmlHandler.getIndex(request, response);
   } else if (parsedUrl.pathname === '/getUsers') {
     jsonHandler.getUsers(request, response);
@@ -53,19 +53,19 @@ const handleGet = (request, response, parsedUrl) => {
 };
 
 const handleHead = (request, response, parsedUrl) => {
-    if (parsedUrl.pathname === '/getUsers') {
-      jsonHandler.getUsersMeta(request, response);
-    } else {
-      jsonHandler.notFoundMeta(request, response);
-    }
-  };
+  if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsersMeta(request, response);
+  } else {
+    jsonHandler.notFoundMeta(request, response);
+  }
+};
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
   if (request.method === 'POST') {
     handlePost(request, response, parsedUrl);
-  } else if (request.method === "HEAD"){
+  } else if (request.method === 'HEAD') {
     handleHead(request, response, parsedUrl);
   } else {
     handleGet(request, response, parsedUrl);
